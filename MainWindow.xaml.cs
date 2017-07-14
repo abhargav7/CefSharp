9df96faa2aa30d652772251f30d0dfb94c2d7342
abhargav7 <er.arpit7@gmail.com>
2017-07-14 17:28:12 +0530

    Minimal steps for Bug to be reproduced
    Hang issue in modal dialog

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+         
+        }
+        void btnOpenModal_Click(object sender, RoutedEventArgs e)
+        {
+            ModalWindow modalWindow = new ModalWindow();
+            modalWindow.ShowDialog();
+
+        }
+    }
+}
---
+++ UserControl1.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+
+        #region Singleton
+        private static volatile UserControl1 instance;
+        private static object syncRoot = new Object();
+
+        public static UserControl1 Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new UserControl1();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+        private UserControl1()
+        {
+            //  Application.Current.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                InitializeComponent();
+                Browser.Address = "http://www.google.com";
+            }
+            this.Loaded += UserControl1_Initialized;
+            this.Unloaded += UserControl1_Unloaded;
+
+        }
+
+        private void UserControl1_Unloaded(object sender, RoutedEventArgs e)
+        {
+            // throw new NotImplementedException();
+        }
+
+        private void UserControl1_Initialized(object sender, EventArgs e)
+        {
+            // throw new NotImplementedException();
+            //uncommentabove issue is gone//
+
+            ////test work
+            //GridIn.Focus(); ;
+            ////GridIn.Children.Clear();
+            //var aq =ChromiumWebBrowser.WebBrowser;
+            //if (aq != null)
+            //{
+            //    ChromiumWebBrowser aChromiumWebBrowser = new ChromiumWebBrowser();
+            //    aChromiumWebBrowser.Address = aq.Address;
+            //    GridIn.Children.Clear();
+            //    if (aq.Address != null)
+            //        ChromiumWebBrowser.WebBrowser = aq;
+
+            //    GridIn.Children.Add(ChromiumWebBrowser);
+        }
+    }
+}
